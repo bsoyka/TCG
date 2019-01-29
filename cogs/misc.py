4150e5defbd14fbcd53ce280a93d40e6147420c5
mathsman5133 <mathsman5132@gmail.com>
2019-01-29 19:44:31 +1100

    combine roles and poll into misc

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,7 +1,10 @@
+from cogs.utils import db
+
 from discord.ext import commands
+
 import discord
 import re
-from cogs.utils import db, checks
+
 import emoji as emoj
 
 
@@ -57,9 +60,72 @@ class UnicodeEmoji:
     def __init__(self, name):
         self.name = name
 
-class Polls:
+
+class Misc:
     def __init__(self, bot):
         self.bot = bot
+
+    MESSAGE_ID = 539680327614857227
+    PYTHON_HELPER = 501896375982751744
+    JAVASCRIPT_HELPER = 501896261310480385
+    ANNOUNCEMENTS_MEMBER = 515920621163642917
+    CODER = 501892380925100043
+
+    USER_COUNT_CHANNEL = 515036392762376212
+    BOT_COUNT_CHANNEL = 515036393362292746
+    MEMBER_COUNT_CHANNEL = 515036392233893888
+    MEMBER_GOAL_CHANNEL = 539709682848235520
+    NEW_MEMBER_CHANNEL = 501873813408186380
+    INFO_CHANNEL = 530941832549367820
+    MEMBER_ROLE = 501872002680750100
+    BOTS_ROLE = 501873192869298176
+
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    async def update_channels(self, ctx):
+        await self._update_channels(ctx.author)
+        await self._update_channels(ctx.me)
+        await ctx.tick()
+
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    async def update_member_goal(self, ctx, number: int):
+        channel = ctx.guild.get_channel(self.MEMBER_GOAL_CHANNEL)
+        await channel.edit(name=f'Member Goal: {number}',
+                           reason=f'Command by {ctx.author.display_name}#{ctx.author.discriminator}')
+        await ctx.tick()
+
+    @commands.command()
+    async def pyhelper(self, ctx):
+        """Give yourself the Python Helper role"""
+        role = ctx.guild.get_role(self.PYTHON_HELPER)
+        await ctx.author.add_roles(role, reason='Auto Role') \
+            if role not in ctx.author.roles else await ctx.author.remove_roles(role, reason='Auto Role')
+        await ctx.tick()
+
+    @commands.command()
+    async def jshelper(self, ctx):
+        """Give yourself the JavaScript Helper role"""
+        role = ctx.guild.get_role(self.JAVASCRIPT_HELPER)
+        await ctx.author.add_roles(role, reason='Auto Role') \
+            if role not in ctx.author.roles else await ctx.author.remove_roles(role, reason='Auto Role')
+        await ctx.tick()
+
+    @commands.command()
+    async def coder(self, ctx):
+        """Give yourself the Coder role"""
+        role = ctx.guild.get_role(self.CODER)
+        await ctx.author.add_roles(role, reason='Auto Role') \
+            if role not in ctx.author.roles else await ctx.author.remove_roles(role, reason='Auto Role')
+        await ctx.tick()
+
+    @commands.command()
+    async def announcements(self, ctx):
+        """Give yourself the Announcements Member role"""
+        role = ctx.guild.get_role(self.ANNOUNCEMENTS_MEMBER)
+        await ctx.author.add_roles(role, reason='Auto Role') \
+            if role not in ctx.author.roles else await ctx.author.remove_roles(role, reason='Auto Role')
+        await ctx.tick()
 
     @commands.group()
     async def poll(self, ctx):
@@ -172,7 +238,98 @@ class Polls:
         await ctx.send(embed=e)
 
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        await self.reaction_role_action(payload, 'add')
         await self.reaction_action(payload)
+
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+        await self.reaction_role_action(payload, 'add')
+
+    async def reaction_role_action(self, payload, add_delete):
+        if payload.message_id != self.MESSAGE_ID:
+            return
+
+        reactions = {'\N{PUBLIC ADDRESS LOUDSPEAKER}': self.ANNOUNCEMENTS_MEMBER,
+                     '\N{DESKTOP COMPUTER}': self.CODER,
+                     'python:513877036058542090': self.PYTHON_HELPER,
+                     'javascript:513877604437065728': self.JAVASCRIPT_HELPER}
+        if str(payload.emoji) not in reactions.keys():
+            return
+
+        role_id = reactions[str(payload.emoji)]
+        guild = self.bot.get_guild(payload.guild_id)
+        role = guild.get_role(role_id)
+        member = guild.get_member(payload.user_id)
+
+        try:
+            await member.add_roles(role, reason='Reaction roles') \
+                if add_delete == 'add' else await member.remove_roles(role, reason='Reaction roles')
+        except:
+            return
+
+        e = discord.Embed(colour=discord.Colour.green())
+        e.description = f"You were {'given' if add_delete == 'add' else 'removed from'}" \
+                        f" the {role.name} role. {'Remove' if add_delete == 'add' else 'Add'} " \
+                        f"the {payload.emoji} reaction to {'remove' if add_delete == 'add' else 'Add'} the role"
+        try:
+            await member.send(embed=e)
+        except:
+            pass
+
+    async def on_member_join(self, member):
+        await self._update_channels(member)
+        await self.member_welcomer(member, True)
+        await self.give_roles(member)
+
+    async def on_member_remove(self, member):
+        await self._update_channels(member)
+        await self.member_welcomer(member, False)
+
+    async def _update_channels(self, member):
+        if member.bot:
+            bot_count = [n for n in member.guild.members if n.bot]
+            channel = member.guild.get_channel(self.BOT_COUNT_CHANNEL)
+            await channel.edit(name=f'Bot Count: {len{bot_count)}', reason='Bot Joined')
+        else:
+            user_count = [n for n in member.guild.members if not n.bot]
+            channel = member.guild.get_channel(self.USER_COUNT_CHANNEL)
+            await channel.edit(name=f'User Count: {len(user_count)}', reason='Member Joined')
+
+        member_count = channel.guild.members
+        channel = member.guild.get_channel(self.MEMBER_COUNT_CHANNEL)
+        await channel.edit(name=f'Member Count: {len(member_count)}', reason="User Joined")
+
+    async def member_welcomer(self, member, join_leave: bool):
+        e = discord.Embed(colour=discord.Colour.green() if join_leave else discord.Colour.red())
+        e.set_author(name='New Member' if join_leave else 'Member Left')
+        e.set_image(url=member.avatar_url)
+
+        if join_leave:
+            e.description = f'Welcome to **The Coding Group**, {member.mention} \nWe hope you enjoy your time here!'
+        else:
+            e.description = f'{member.mention} just left. :cry:'
+
+        e.set_footer(text=f'Member Count: {len(member.guild.members)}')
+
+        channel = member.guild.get_channel(self.NEW_MEMBER_CHANNEL)
+        await channel.send(embed=e)
+
+        if member.bot and join_leave:
+            e = discord.Embed(colour=discord.Colour.green())
+            e.description = 'Welcome! Please make sure you check out the rules ' \
+                            f'and any reaction roles you would like in <#{self.INFO_CHANNEL}>. ' \
+                            f'\n\nWe hope you enjoy your stay!'
+            try:
+                await member.send(embed=e)
+            except:
+                pass
+
+    async def give_roles(self, member):
+        if member.bot:
+            role = member.guild.get_role(self.BOTS_ROLE)
+            await member.add_roles(role, reason='Auto Role on Join')
+        else:
+            role = member.guild.get_role(self.MEMBER_ROLE)
+            await member.add_roles(role, reason='Auto Role on Join')
 
     async def reaction_action(self, payload):
         author = self.bot.get_user(payload.user_id)
@@ -255,6 +412,4 @@ class Polls:
 
 
 def setup(bot):
-    bot.add_cog(Polls(bot))
-
-
+    bot.add_cog(Misc(bot))
